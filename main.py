39d2a4e3cad646aaa72a6d49f2e4da908a15e7d4
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2019-11-06 22:34:43 +0800

    position and cash output OK

--- main.py
+++ main.py
@@ -9,13 +9,16 @@
 from itertools import chain, groupby
 from functools import partial
 from utils.utility import dictToValues, writeCsv
-from utils.iter import pop
+from utils.iter import pop, firstOf
 from toolz.functoolz import compose
+from toolz.itertoolz import groupby as groupbyToolz
 from nomura.main import fileToLines, getHeadersnLines, getCashHeaders \
 						, getHoldingHeaders, getOutputFileName
 from os.path import join, dirname, abspath
+import re
 import logging
 logger = logging.getLogger(__name__)
+
 
 
 
@@ -30,7 +33,7 @@ holdingPosition = lambda date, p: \
 	( { 'portfolio': p['Custody Account Name']
 	  , 'custodian': ''
 	  , 'date': date
-	  , 'name': p['Securities name']
+	  , 'name': p['Securities name'].strip()
 	  , 'currency': p['Market Price Currency']
 	  , 'quantity': p['Holding']
 	  }
@@ -46,11 +49,11 @@ holdingPosition = lambda date, p: \
 		[Dictionary] Geneva cash position
 """
 cashPosition = lambda date, p: \
-	{ 'portfolio': p['Portfolio']\
-	, 'custodian': ''\
-	, 'date': date\
-	, 'currency': p['account_ccy_code']\
-	, 'balance': stringToFloat(p['ledger_bal_in_acct_ccy'])\
+	{ 'portfolio': p['Account Name']
+	, 'custodian': ''
+	, 'date': date
+	, 'currency': p['Currency']
+	, 'balance': p['Current Ledger Balance']
 	}
 
 
@@ -134,6 +137,37 @@ def lookupSecurityId(idType, idNumber):
 
 
 
+filenameWithoutPath = lambda filename: \
+	filename.split('\\')[-1]
+
+
+
+"""
+	[String] fn => [String] date (yyyy-mm-dd)
+
+	Date can either appear as yyyy-mm-dd or ddmmyyyy in the file name, like:
+
+	BOC Broker Statement 2019-11-04 (A-MC).xls
+	Cash Stt _04112019.xlsx
+	BOC Bank Statement 2019-11-01 (Class A-MC BOND) (HKD).xls
+"""
+dateFromFilename = lambda fn: \
+	(lambda m: \
+		(lambda s: s[4:8] + '-' + s[2:4] + '-' + s[0:2])(m.group(0)) \
+		if m != None else \
+		(lambda m2: \
+			m2.group(0)	if m2 != None else throwValueError('dateFromFilename(): {0}'.format(fn))
+		)(re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}', fn))
+	)(re.search('[0-9]{8}', fn))
+
+
+
+def throwValueError(msg):
+	logger.error(msg)
+	raise ValueError
+
+
+
 def skip2(it):
 	"""
 	[Iterable] it => [Iterable] it, with the first 2 elements skipped
@@ -147,19 +181,6 @@ def skip2(it):
 
 
 """
-	[Iterable] group of lines => [Iterable] single line
-
-	For each holding position, the "Holding Details" line (1st line in group) 
-	contains trade day quantity, the "Sub-Total" line (2nd line in group) 
-	contains currency, price etc. We need to merge them into a single line.
-"""
-consolidate = lambda group: \
-	map( lambda t: t[0] if t[0] != '' else t[1]
-	   , zip(pop(group), pop(group)))
-
-
-
-"""
 	[String] file => [Iterable] positions
 
 	position: [Dictionary] header -> value
@@ -167,7 +188,8 @@ consolidate = lambda group: \
 getRawHoldingPositions = compose(
 	  lambda t: map(lambda line: dict(zip(t[0], line)), t[1])
 	, lambda t: ( t[0]
-				, map( lambda keynGroup: consolidate(keynGroup[1])
+				, map( lambda keynGroup: \
+						firstOf(lambda line: line[0] == 'Sub-Total' , keynGroup[1])
 	   				 , groupby(t[1], lambda line: line[8]))
 				)
 	, lambda t: ( t[0]
@@ -177,6 +199,44 @@ getRawHoldingPositions = compose(
 	, skip2
 	, fileToLines
 )
+
+
+
+"""
+	[String] file => [Iterable] cash positions
+
+	We need to handle two challenges here:
+
+	(1) cash belonging to the same account number can show up in multiple
+		lines.
+	(2) cash of the same currency can belong to multiple accounts.
+"""
+getRawCashHoldings = compose(
+	  lambda t: map(lambda line: dict(zip(t[0], line)), t[1])
+	, lambda t: ( t[0]
+				, map( consolidate
+					 , groupbyToolz(lambda line: line[3], t[1]).values())
+				)
+	, lambda t: ( t[0]
+				, map( lambda keynGroup: pop(keynGroup[1])
+					 , groupby(t[1], lambda line: line[1]))
+				)
+	, getHeadersnLines
+	, fileToLines
+)
+
+
+
+def consolidate(lineGroup):
+	"""
+	[List] lineGroup => [Iterable] line
+
+	where lineGroup is a group of lines of the same currency, we need to
+	add up their ledger balance
+	"""
+	r = lineGroup[0].copy()
+	r[7] = sum(map(lambda L: L[7], lineGroup))
+	return r
 
 
 
@@ -197,7 +257,6 @@ if __name__ == '__main__':
 	import logging.config
 	logging.config.fileConfig('logging.config', disable_existing_loggers=False)
 
-	inputFile = join(getCurrentDirectory(), 'samples', 'Holding _17102019.xlsx')
-	for p in map( partial(holdingPosition, '2019-10-17')
-				, getRawHoldingPositions(inputFile)):
-		print(p)
+	inputFile = join(getCurrentDirectory(), 'samples', 'Cash Stt _04112019.xlsx')
+	for p in getRawCashHoldings(inputFile):
+		print(p['Account Number'], p['Currency'], p['Current Ledger Balance'])
--- test/test_ALL.py
+++ test/test_ALL.py
@@ -4,7 +4,11 @@
 import unittest2
 from os.path import join
 from utils.iter import firstOf
-from bochk_revised.main import getCurrentDirectory, getRawHoldingPositions
+from functools import partial
+from bochk_revised.main import getCurrentDirectory, getRawHoldingPositions\
+								, holdingPosition, dateFromFilename\
+								, filenameWithoutPath, cashPosition\
+								, getRawCashHoldings
 
 
 
@@ -24,11 +28,139 @@ class TestALL(unittest2.TestCase):
 
 
 
+	def testDateFromFilename(self):
+		self.assertEqual( '2019-11-04'
+						, dateFromFilename('BOC Broker Statement 2019-11-04 (A-MC).xls'))
+		self.assertEqual( '2019-11-04'
+						, dateFromFilename('Cash Stt _04112019.xlsx'))
+		self.assertEqual( '2019-11-01'
+						, dateFromFilename('BOC Bank Statement 2019-11-01 (Class A-MC BOND) (HKD).xls'))
+
+
+
+	def testHoldingPosition(self):
+		inputFile = join(getCurrentDirectory(), 'samples', 'Holding _17102019.xlsx')
+		positions = list(map( partial(holdingPosition, dateFromFilename(filenameWithoutPath(inputFile)))
+							, getRawHoldingPositions(inputFile)))
+		self.assertEqual(19, len(positions))
+		self.verifyPosition(
+			firstOf(lambda p: p['name'] == 'CMHI FINA BVI CO 06/08/2023', positions))
+		self.verifyPosition2(
+			firstOf(lambda p: p['name'] == 'CHINA CINDA FINANCE 4.25 23/04/25', positions))
+
+
+
+	def testHoldingPosition2(self):
+		inputFile = join(getCurrentDirectory(), 'samples', 'BOC Broker Statement 2019-11-01.xls')
+		positions = list(map( partial(holdingPosition, dateFromFilename(filenameWithoutPath(inputFile)))
+							, getRawHoldingPositions(inputFile)))
+		self.assertEqual(159, len(positions))
+		self.verifyPosition3(
+			firstOf(lambda p: p['name'] == 'NWD MTN LTD 6 18/09/2023', positions))
+
+
+
+	def testHoldingPosition3(self):
+		inputFile = join(getCurrentDirectory(), 'samples', 'BOC Broker Statement 2019-11-04 (A-MC).xls')
+		positions = list(map( partial(holdingPosition, dateFromFilename(filenameWithoutPath(inputFile)))
+							, getRawHoldingPositions(inputFile)))
+		self.assertEqual(55, len(positions))
+		self.verifyPosition4(
+			firstOf(lambda p: p['name'] == 'CHINA STATE CON 6 31/12/49', positions))
+
+
+
+	def testCashPosition(self):
+		inputFile = join(getCurrentDirectory(), 'samples', 'Cash Stt _04112019.xlsx')
+		positions = list(map( partial(cashPosition, dateFromFilename(filenameWithoutPath(inputFile)))
+							, getRawCashHoldings(inputFile)))
+		self.assertEqual(4, len(positions))
+		self.verifyCashPosition(
+			firstOf(lambda p: p['currency'] == 'CNY', positions))
+		self.verifyCashPosition2(
+			firstOf(lambda p: p['currency'] == 'USD', positions))
+		self.verifyCashPosition3(
+			firstOf(lambda p: p['currency'] == 'HKD', positions))
+
+
+
 	def verifyRawPosition(self, position):
 		self.assertEqual('DIANJIAN HAIYU LTD 3.5 31/05/22', position['Securities name'])
-		self.assertEqual( 3000000.00 , position['Holding'])
+		self.assertEqual(3000000.00 , position['Holding'])
 		self.assertEqual('USD', position['Market Price Currency'])
 		self.assertAlmostEqual(100.047, position['Market Unit Price'])
 		self.assertEqual('ISIN', position['Securities ID Type'])
 		self.assertEqual( 'CHINA LIFE FRANKLIN ASSET MANAGEMENT CO LTD'
 						, position['Custody Account Name'])
+
+
+
+	def verifyPosition(self, position):
+		self.assertEqual('CHINA LIFE FRANKLIN ASSET MANAGEMENT CO LTD', position['portfolio'])
+		self.assertEqual('', position['custodian'])
+		self.assertEqual('2019-10-17', position['date'])
+		self.assertEqual('USD', position['currency'])
+		self.assertEqual(3000000.00, position['quantity'])
+		self.assertEqual('XS1856799421', position['ISIN'])
+		self.assertEqual('', position['bloomberg_figi'])
+		self.assertEqual('', position['geneva_investment_id'])
+
+
+
+	def verifyPosition2(self, position):
+		self.assertEqual('CHINA LIFE FRANKLIN ASSET MANAGEMENT CO LTD', position['portfolio'])
+		self.assertEqual('', position['custodian'])
+		self.assertEqual('2019-10-17', position['date'])
+		self.assertEqual('USD', position['currency'])
+		self.assertEqual(1000000.00, position['quantity'])
+		self.assertEqual('', position['ISIN'])
+		self.assertEqual('', position['bloomberg_figi'])
+		self.assertEqual('USG21184AB52 HTM', position['geneva_investment_id'])
+
+
+
+	def verifyPosition3(self, position):
+		self.assertEqual('CLT - CLI HK BR (CLASS A-HK) TRUST FD (BOND)- PAR', position['portfolio'])
+		self.assertEqual('', position['custodian'])
+		self.assertEqual('2019-11-01', position['date'])
+		self.assertEqual('HKD', position['currency'])
+		self.assertEqual(200000000, position['quantity'])
+		self.assertEqual('', position['ISIN'])
+		self.assertEqual('', position['bloomberg_figi'])
+		self.assertEqual('HK0000163607 HTM', position['geneva_investment_id'])
+
+
+
+	def verifyPosition4(self, position):
+		self.assertEqual('CLT - CLI MACAU BR (CLASS A-MC) TRUST FUND (BOND)', position['portfolio'])
+		self.assertEqual('', position['custodian'])
+		self.assertEqual('2019-11-04', position['date'])
+		self.assertEqual('USD', position['currency'])
+		self.assertEqual(20000000, position['quantity'])
+		self.assertEqual('XS1912494538', position['ISIN'])
+		self.assertEqual('', position['bloomberg_figi'])
+		self.assertEqual('', position['geneva_investment_id'])
+
+
+
+	def verifyCashPosition(self, position):
+		self.assertEqual('CHINA LIFE FRANKLIN ASSET MANAGEMENT CO LTD', position['portfolio'])
+		self.assertEqual('', position['custodian'])
+		self.assertEqual('2019-11-04', position['date'])
+		self.assertAlmostEqual(6.91, position['balance'])
+
+
+
+	def verifyCashPosition2(self, position):
+		self.assertEqual('CHINA LIFE FRANKLIN ASSET MANAGEMENT CO LTD', position['portfolio'])
+		self.assertEqual('', position['custodian'])
+		self.assertEqual('2019-11-04', position['date'])
+		self.assertAlmostEqual(148465.26, position['balance'])
+
+
+
+	def verifyCashPosition3(self, position):
+		self.assertEqual('CHINA LIFE FRANKLIN ASSET MANAGEMENT CO LTD', position['portfolio'])
+		self.assertEqual('', position['custodian'])
+		self.assertEqual('2019-11-04', position['date'])
+		self.assertAlmostEqual(38.52, position['balance'])
